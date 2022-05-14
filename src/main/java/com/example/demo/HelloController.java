c7c1ede19a271e801c20e994550c8a284d5f9f02
小山由人 <koyama@reytech.co.jp>
2022-05-14 20:19:31 +0900

    修正案の作成

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping(value = "/hello")
-	public String hello() {
-		return "/index.html";
+	@GetMapping("/hello")
+	public String hello(Model model) {
+		model.addAttribute("message", "Hello World");
+		return "index";
 	}
+
 }
